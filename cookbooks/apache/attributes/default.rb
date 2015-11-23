b0d8b9540d19d4053b5506810a9bde4f21d34a74
Michael Carper <mcarper@virtusa.com>
2015-11-23 14:02:36 +0000

    Updating our workstation chef-repo

--- cookbooks/apache/attributes/default.rb
+++ cookbooks/apache/attributes/default.rb
@@ -2,6 +2,8 @@ default["apache"]["sites"]["michaelwcarper2"] = { "site_title" => "Michael's web
 default["apache"]["sites"]["michaelwcarper2b"] = { "site_title" => "Michael's website2b", "port" => 80, "domain" => "michaelwcarper2b.mylabserver.com" }
 default["apache"]["sites"]["michaelwcarper4"] = { "site_title" => "anthony4 website", "port" => 80, "domain" => "michaelwcarper4.mylabserver.com" }
 
+default["author"]["name"] = "michael"
+
 case node["platform"]
 when "centos"
 	default["apache"]["package"] = "httpd"
